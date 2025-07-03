3ef0c8acedbe395908ba85e15843b0b64289c10b
mbero_akoko <mberoakoko@gmail.com>
2025-07-04 01:15:51 +0200

    added some additional code

--- include/utils/functional_utils.hpp
+++ include/utils/functional_utils.hpp
@@ -157,10 +157,92 @@ namespace PlexiStruct::functional {
         };
     };
 
+    enum class expr_kind: std::uint8_t {
+        ILLEGAL = 0, OPEARATOR, VALUE
+    };
+
+    struct expr_node_item {
+        expr_kind kind { expr_kind::ILLEGAL };
+        double value { 0 };
+        Operator op { Operator::PLUS };
+
+        static auto create(const double& value) -> expr_node_item {
+           return {.kind = expr_kind::VALUE, .value = value};
+        }
+
+        static auto create(const Operator& op) -> expr_node_item {
+            return {.kind = expr_kind::OPEARATOR, .op = op};
+        }
+    };
+
+    class FlattenedExpresstionVisitor final: public IExprVisitor {
+        std::list<expr_node_item> nodes_ {};
+
+        static auto make_item(const double& number) -> expr_node_item {
+            return expr_node_item::create(number);
+        }
+
+        static auto make_item(const Operator& op) -> expr_node_item {
+            return  expr_node_item::create(op);
+        }
+    public:
+        explicit FlattenedExpresstionVisitor() = default;
+        ~FlattenedExpresstionVisitor() override;
+
+        auto accept(const Number &number) -> double override {
+            nodes_.push_back(make_item(number.get_num()));
+            return number.get_num();
+        };
+
+        auto accept(const UnaryExpression &unary_expr) -> double override {
+            unary_expr.get_operand()->accept(*this);
+            nodes_.push_back(make_item(unary_expr.get_op()));
+            return 43;
+        };
+
+        auto accept(const BinaryExpression &binary_expr) -> double override {
+            binary_expr.get_right()->accept(*this);
+            binary_expr.get_left()->accept(*this);
+            nodes_.push_back(make_item(binary_expr.get_op()));
+            return 42;
+        };
+
+        auto get_expression_list() -> std::list<expr_node_item> {
+            return nodes_;
+        }
+    };
+
+    inline auto gen_expression_list(const ExpPtr& expression) -> std::list<expr_node_item> {
+        std::shared_ptr<IExprVisitor> evaluator = std::make_unique<FlattenedExpresstionVisitor>();
+        expression->accept(*evaluator);
+        using FlatteneExprVisitorPtr = std::shared_ptr<FlattenedExpresstionVisitor>;
+        return  static_cast<FlatteneExprVisitorPtr>(evaluator)->get_expression_list();
+    }
+
+    class stack: public std::stack<double> {
+    public:
+        stack() = default;
+        auto push_data(const double& value) -> void {
+            this->push(value);
+        }
+
+        auto pop_data() -> double {
+            auto value = this->top();
+            this->pop();
+            return value;
+        }
+    };
+
+    inline auto evaluate(const std::list<expr_node_item>& expr_list) -> double {
+
+    }
+
     inline auto evaluate(std::shared_ptr<IExpr> expr) -> double {
         const auto evaluator = std::make_shared<TreeEvalVisitor>();
         return expr->accept(*evaluator);
     }
+
+    inline auto
 
     inline auto test_evaluation() -> void  {
         ExpPtr exp = Number::make(1);
